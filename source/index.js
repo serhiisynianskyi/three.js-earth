e70f49d77199cc33fba8ed7e797921d36c2f6aa9
Sergius <kfzadrser@gmail.com>
2018-04-05 00:58:33 +0300

    update(index.js): add shaders, points on planet

--- source/index.js
+++ source/index.js
@@ -2,12 +2,19 @@
 import * as THREE from 'three';
 import earthMap from './media/images/earth-map.jpg';
 import earthMapBump from './media/images/earthbump.jpg';
+import vertexShader from './vertex-shader.js'
+import fragmentShader from './fragment-shader.js'
 
 let OrbitControls = require('three-orbit-controls')(THREE);
 
-let canvas = document.getElementById('main-canvas');
-let scene = new THREE.Scene();
-let camera, renderer, light, controls, stats;
+let canvas = document.getElementById('main-canvas'),
+	scene = new THREE.Scene(),
+	camera, renderer, light, controls, stats,
+	time = 0,
+	R = 26,
+	planes = [],
+	group = new THREE.Group();
+scene.add(group);
 
 initScene();
 addLights();
@@ -30,6 +37,48 @@ function initScene() {
 	renderer.setClearColor(0x000000);
 }
 
+let points = [{
+		title: 'Bali',
+		lat: -8.409518,
+		long: 115.188919
+	},
+	{
+		title: 'Yevpatoriya',
+		lat: 45.20091,
+		long: 33.36655
+	},
+	{
+		title: 'Kyiv',
+		lat: 50.431782,
+		long: 30.516382
+	},
+	{
+		title: 'New York',
+		lat: 40.730610,
+		long: -73.968285
+	},
+	{
+		title: 'Managua',
+		lat: 12.136389,
+		long: -86.251389
+	},
+	{
+		title: 'Sydney',
+		lat: -33.865143,
+		long: 151.215256
+	},
+	{
+		title: 'Berlin',
+		lat: 52.520645,
+		long: 13.409779
+	},
+	{
+		title: 'Lisboa',
+		lat: 38.736946,
+		long: -9.142685
+	}
+];
+
 function resize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -44,17 +93,17 @@ function addLights() {
 function createEarth() {
 	let geometry = new THREE.SphereGeometry(25, 132, 132),
 		material = new THREE.MeshPhongMaterial({
-		map: THREE.ImageUtils.loadTexture(earthMap),
-		displacementMap: THREE.ImageUtils.loadTexture(earthMapBump),
-		displacementScale: 1.5,
-		// map: THREE.ImageUtils.loadTexture(image),
-		// alphaMap: THREE.ImageUtils.loadTexture(imageBump),
-		// transparent : true,
-		// depthWrite  : false
-		// displacementMap: THREE.ImageUtils.loadTexture(imageBump),
-		// displacementScale: 10,
-		// normalScale: 10
-	});
+			map: THREE.ImageUtils.loadTexture(earthMap),
+			displacementMap: THREE.ImageUtils.loadTexture(earthMapBump),
+			displacementScale: 1.5,
+			// map: THREE.ImageUtils.loadTexture(image),
+			// alphaMap: THREE.ImageUtils.loadTexture(imageBump),
+			// transparent : true,
+			// depthWrite  : false
+			// displacementMap: THREE.ImageUtils.loadTexture(imageBump),
+			// displacementScale: 10,
+			// normalScale: 10
+		});
 	let sphere = new THREE.Mesh(geometry, material);
 	scene.add(sphere);
 }
@@ -62,6 +111,20 @@ function createEarth() {
 controls = new OrbitControls(camera);
 
 function rendering() {
+	time++;
+	// group.rotation.x = time/100;
+	planes.forEach(e => {
+		let conj = new THREE.Quaternion();
+		conj.copy(group.quaternion);
+		conj.conjugate();
+
+		e.quaternion.multiplyQuaternions(
+			conj,
+			camera.quaternion
+		);
+
+		// e.quaternion.copy(camera.quaternion);
+	});
 	requestAnimationFrame(rendering);
 	renderer.render(scene, camera);
 };
@@ -70,4 +133,41 @@ window.addEventListener('resize', function(e) {
 	resize();
 });
 
+points.forEach(p => {
+	let pos = calcPosFromLatLonRad(p.lat, p.long, R);
+	let geometry = new THREE.PlaneGeometry(1, 1);
+	let material = new THREE.MeshBasicMaterial({
+		color: 0x00ff00,
+		side: THREE.DoubleSide,
 
+		// wireframe: true
+	});
+	let material1 = new THREE.RawShaderMaterial({
+		uniforms: {
+			time: { value: 0 },
+			hover: { value: 1 }
+		},
+		transparent: true,
+		vertexShader: vertexShader(),
+		fragmentShader: fragmentShader()
+	});
+	let plane = new THREE.Mesh(geometry, material1);
+	plane.position.x = pos[0];
+	plane.position.y = pos[1];
+	plane.position.z = pos[2];
+	group.add(plane);
+	planes.push(plane);
+});
+
+
+function calcPosFromLatLonRad(lat, lon, radius) {
+
+	var phi = (90 - lat) * (Math.PI / 180);
+	var theta = (lon + 180) * (Math.PI / 180);
+
+	let x = -((radius) * Math.sin(phi) * Math.cos(theta));
+	let z = ((radius) * Math.sin(phi) * Math.sin(theta));
+	let y = ((radius) * Math.cos(phi));
+	// console.log([x, y, z]);
+	return [x, y, z];
+}
